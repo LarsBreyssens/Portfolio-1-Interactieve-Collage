129385a09aaf392e547c1ef91a330ed5e8ec2418
LarsBreyssens <Lars.Breyssens@student.ehb.be>
2026-04-03 00:23:56 +0200

    zee

--- Interactievecollage.js
+++ Interactievecollage.js
@@ -5,6 +5,7 @@ function setup() {
 function draw() {
   background(173, 216, 230);
   drawMountains();
+
   drawCloud(150, 120, 1.2);
   drawCloud(400, 80, 0.9);
   drawCloud(600, 160, 1.5);
@@ -32,4 +33,14 @@ function drawCloud(x, y, scale) {
   ellipse(x + 25 * scale, y + 10 * scale, 50 * scale, 40 * scale);
   ellipse(x - 25 * scale, y + 10 * scale, 50 * scale, 40 * scale);
   ellipse(x, y + 20 * scale, 55 * scale, 35 * scale);
+}
+
+if (zee) {
+  image(zee, 0, 500, width, 100);
+}
+
+let zee;
+
+function preload() {
+  zee = loadImage("images/zee.png");
 }
