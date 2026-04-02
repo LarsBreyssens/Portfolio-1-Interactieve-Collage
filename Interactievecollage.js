09bd7fe1ab4f00bb0fb6cfff791426f688cb0b36
LarsBreyssens <Lars.Breyssens@student.ehb.be>
2026-04-03 01:01:06 +0200

    vogel2

--- Interactievecollage.js
+++ Interactievecollage.js
@@ -2,7 +2,7 @@ function setup() {
   createCanvas(800, 600);
 }
 
-let vogelX = 0;
+let vogel = 0;
 
 function draw() {
   background(173, 216, 230);
@@ -16,74 +16,73 @@ function draw() {
   drawSea();
   drawSeaLight();
 
-  drawBird(vogelX, 60);
-  vogelX += 2;
-  if (vogelX > width + 40) {
-    vogelX = -40;
+  drawBird(vogel, 60);
+  vogel += 2;
+  if (vogel > width + 40) {
+    vogel = -40;
   }
+}
 
-  function drawBird(x, y) {
-    push();
-    stroke(60, 60, 60);
-    strokeWeight(3);
-    noFill();
+function drawBird(x, y) {
+  push();
+  stroke(60, 60, 60);
+  strokeWeight(3);
+  noFill();
+  beginShape();
+  vertex(x - 20, y + 5);
+  vertex(x, y);
+  vertex(x + 20, y + 5);
+  endShape();
+  pop();
+}
 
-    beginShape();
-    vertex(x - 20, y + 5);
+function drawSea() {
+  noStroke();
+  fill(0, 105, 180, 220);
+  let waveHeight = 28;
+  let waveLength = 120;
+  let speed = millis() * 0.002;
+  let seaTop = height - 140;
+  beginShape();
+  vertex(0, height);
+  for (let x = 0; x <= width; x += 10) {
+    let y = seaTop + sin((x / waveLength) * TWO_PI + speed) * waveHeight;
     vertex(x, y);
-    vertex(x + 20, y + 5);
-    endShape();
-    pop();
   }
+  vertex(width, height);
+  endShape(CLOSE);
+}
 
-  function drawSea() {
-    noStroke();
-    fill(0, 105, 180, 220);
-    let waveHeight = 28;
-    let waveLength = 120;
-    let speed = millis() * 0.002;
-    let seaTop = height - 140;
-    beginShape();
-    vertex(0, height);
-    for (let x = 0; x <= width; x += 10) {
-      let y = seaTop + sin((x / waveLength) * TWO_PI + speed) * waveHeight;
-      vertex(x, y);
-    }
-    vertex(width, height);
-    endShape(CLOSE);
+function drawSeaLight() {
+  noStroke();
+  fill(100, 180, 230, 180);
+  let waveHeight = 18;
+  let waveLength = 90;
+  let speed = millis() * 0.002 + 100;
+  let seaTop = height - 80;
+  beginShape();
+  vertex(0, height);
+  for (let x = 0; x <= width; x += 10) {
+    let y = seaTop + sin((x / waveLength) * TWO_PI + speed) * waveHeight;
+    vertex(x, y);
   }
+  vertex(width, height);
+  endShape(CLOSE);
+}
 
-  function drawSeaLight() {
-    noStroke();
-    fill(100, 180, 230, 180);
-    let waveHeight = 18;
-    let waveLength = 90;
-    let speed = millis() * 0.002 + 100;
-    let seaTop = height - 80;
-    beginShape();
-    vertex(0, height);
-    for (let x = 0; x <= width; x += 10) {
-      let y = seaTop + sin((x / waveLength) * TWO_PI + speed) * waveHeight;
-      vertex(x, y);
-    }
-    vertex(width, height);
-    endShape(CLOSE);
-  }
+function drawMountains() {
+  noStroke();
+  fill(220, 270, 100);
+  triangle(0, 500, 200, 200, 400, 500);
 
-  function drawMountains() {
-    noStroke();
-    fill(220, 270, 100);
-    triangle(0, 500, 200, 200, 400, 500);
+  fill(200, 240, 180);
+  triangle(300, 500, 500, 250, 700, 500);
 
-    fill(200, 240, 180);
-    triangle(300, 500, 500, 250, 700, 500);
+  fill(180, 220, 240);
+  triangle(600, 500, 750, 300, 900, 500);
 
-    fill(180, 220, 240);
-    triangle(600, 500, 750, 300, 900, 500);
-
-    fill(250, 300, 220);
-    triangle(100, 600, 350, 350, 600, 600);
-  }
+  fill(250, 300, 220);
+  triangle(100, 600, 350, 350, 600, 600);
 }
 
 function drawHoverCloud(x, y, scale) {
