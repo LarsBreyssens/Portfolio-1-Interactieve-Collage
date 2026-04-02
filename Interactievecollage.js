438604d18c0d05291e9deb4bb9f02ddcbcd7a95d
LarsBreyssens <Lars.Breyssens@student.ehb.be>
2026-04-03 00:14:04 +0200

    Bergen

--- Interactievecollage.js
+++ Interactievecollage.js
@@ -4,9 +4,25 @@ function setup() {
 
 function draw() {
   background(173, 216, 230);
+  drawMountains();
   drawCloud(150, 120, 1.2);
   drawCloud(400, 80, 0.9);
   drawCloud(600, 160, 1.5);
+
+  function drawMountains() {
+    noStroke();
+    fill(120, 170, 200);
+    triangle(0, 500, 200, 200, 400, 500);
+
+    fill(100, 140, 180);
+    triangle(300, 500, 500, 250, 700, 500);
+
+    fill(80, 120, 160);
+    triangle(600, 500, 750, 300, 900, 500);
+
+    fill(150, 200, 220);
+    triangle(100, 600, 350, 350, 600, 600);
+  }
 }
 
 function drawCloud(x, y, scale) {
