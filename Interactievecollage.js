ea3829d58ec87c59bddf3b7d22b3c805fb83c27f
LarsBreyssens <Lars.Breyssens@student.ehb.be>
2026-04-03 00:01:24 +0200

    wolken

--- Interactievecollage.js
+++ Interactievecollage.js
@@ -4,4 +4,16 @@ function setup() {
 
 function draw() {
   background(173, 216, 230);
+  drawCloud(150, 120, 1.2);
+  drawCloud(400, 80, 0.9);
+  drawCloud(600, 160, 1.5);
+}
+
+function drawCloud(x, y, scale) {
+  noStroke();
+  fill(255, 255, 255, 230);
+  ellipse(x, y, 60 * scale, 50 * scale);
+  ellipse(x + 25 * scale, y + 10 * scale, 50 * scale, 40 * scale);
+  ellipse(x - 25 * scale, y + 10 * scale, 50 * scale, 40 * scale);
+  ellipse(x, y + 20 * scale, 55 * scale, 35 * scale);
 }
